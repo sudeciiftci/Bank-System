84ade0009b720ba71bb78f37bd1ee1bac677549e
sudeciiftci <sudeciiftci@gmail.com>
2026-07-08 20:46:45 +0300

    feat: implement withdraw method and test exception handling

--- Main.java
+++ Main.java
@@ -1,3 +1,38 @@
 public class Main {
-    
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount("Sude", 123, 1000);
+
+        try {
+            bankAccount.deposit(500);
+            System.out.println("Balance: " + bankAccount.getBalance());
+        } catch (InvalidAmountException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            bankAccount.deposit(-100);
+        } catch (InvalidAmountException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            bankAccount.withdraw(200);
+            System.out.println("Balance: " + bankAccount.getBalance());
+        } catch (InvalidAmountException | InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            bankAccount.withdraw(-50);
+        } catch (InvalidAmountException | InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            bankAccount.withdraw(5000);
+        } catch (InvalidAmountException | InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
